0540956b278cc469b21afdbd65cb4a0ff2886ac4
Henrik Aalto <hltk@hltk.fi>
2020-12-05 09:33:18 +0200

    solve 05 in ruby

---
+++ 2020/ruby/05.rb
@@ -0,0 +1,9 @@
+seats = ARGF.map{|line|
+  line.tr('FLBR', '0011').to_i 2
+}
+
+p seats.max
+p (seats.min..seats.max).filter{|i| seats.include?(i-1) &&
+                                   !seats.include?(i) &&
+                                    seats.include?(i+1)
+}.first
