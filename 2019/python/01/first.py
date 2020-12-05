9ad0dfa0a356bf205628929287df48e9ab619f01
Henrik Aalto <hltk@hltk.fi>
2020-12-05 07:51:48 +0200

    update 2019/python

--- 2019/python/01/first.py
+++
@@ -1,3 +0,0 @@
-f = lambda x : (x // 3) - 2
-u = list(map(lambda x : int(x.strip()), open('input').readlines()))
-print(sum(map(f, u)))
--- 2019/python/25/o.py
+++
@@ -1,9 +0,0 @@
-u=open('input').readlines()[0].split(',')
-for c in u:
-   try:
-      c = int(c)
-      c = chr(c)
-      print(c, end='')
-   except:
-      pass
-   
