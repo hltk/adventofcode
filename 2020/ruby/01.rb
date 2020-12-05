863f7f2545a6dd01947d812d1fbcb4551cecdbfd
Henrik Aalto <hltk@hltk.fi>
2020-12-05 09:39:59 +0200

    shorten 01.rb

--- 2020/ruby/01.rb
+++ 2020/ruby/01.rb
@@ -1,4 +1,5 @@
 l = ARGF.map &:to_i
 
-p l.combination(2).filter{|s| s.sum == 2020}.first.reduce(1, :*)
-p l.combination(3).filter{|s| s.sum == 2020}.first.reduce(1, :*)
+(2..3).each{|n|
+  p l.combination(n).find{|s| s.sum == 2020}.inject :*
+}
