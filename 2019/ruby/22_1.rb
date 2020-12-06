6f56b23379ce32e1a4c78f533e648c96353e5f69
Henrik Aalto <hltk@hltk.fi>
2020-12-06 13:29:25 +0200

    fix 22_1.rb

--- 2019/ruby/22_1.rb
+++ 2019/ruby/22_1.rb
@@ -7,7 +7,7 @@ pos = ModInt.new(2019)
 read(22, 2019).lines.each{|x|
   case x
   when /stack/ then pos = -pos + ModInt.new(M-1)
-  when /cut (.*)/ then pos-=ModInt.new($1.to_i)
+  when /cut (.*)/ then pos -= ModInt.new($1.to_i)
   when /increment (.*)/ then pos *= ModInt.new($1.to_i)
   end
 }
