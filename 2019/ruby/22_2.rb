fb0ae9c64de9bc961f268d8e62b48f045d0ae2dd
Henrik Aalto <hltk@hltk.fi>
2020-12-06 13:30:35 +0200

    improve 22_2.rb

--- 2019/ruby/22_2.rb
+++ 2019/ruby/22_2.rb
@@ -1,7 +1,6 @@
 require './utils.rb'
 
 M = 119315717514047
-
 times = 101741582076661
 
 a, b = ModInt.new(1), ModInt.new(0)
@@ -18,8 +17,8 @@ read(22,2019).lines.reverse_each{|x|
 # x_2 = a^2x+ab + b
 # x_3 = a^3x     +a^2b + ab + b
 # ...
-
 # \sum{i=0}^k a^ib = (1 - a^n) / (1 - a) * b
 
-x = ModInt.new(2020)
-p x * a.pow(times) + b * (a.pow(times) - ModInt.new(1)) / (a - ModInt.new(1))
+pos = ModInt.new(2020)
+one = ModInt.new(1)
+p pos * a.pow(times) + b * (a.pow(times) - one) / (a - one)
