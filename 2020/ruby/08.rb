3d868720b2dbd58af1cb805528bc8a9ab6ee0f5d
Henrik Aalto <hltk@hltk.fi>
2020-12-08 11:07:15 +0200

    improve ruby vm

--- 2020/ruby/08.rb
+++ 2020/ruby/08.rb
@@ -1,17 +1,20 @@
 require './utils.rb'
 l=read(8).split "\n"
+
 it = 0
 acc = 0
 seen = Set.new
+
 while !seen.include?(it)
   seen.add it
   a, b = l[it].split
   case a
-  when 'jmp' then it += b.to_i;
-  when 'acc' then acc += b.to_i; it += 1;
-  when 'nop' then it += 1;
+  when 'jmp' then it += b.to_i - 1
+  when 'acc' then acc += b.to_i
   end
+  it += 1;
 end
+
 puts acc
 
 (0...l.size).each{|i|
