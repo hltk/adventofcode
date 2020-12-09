674e039b14d4df93fbad225fc38828dbd758c0b2
Henrik Aalto <hltk@hltk.fi>
2020-12-09 07:22:28 +0200

    improve 09.rb

--- 2020/ruby/09.rb
+++ 2020/ruby/09.rb
@@ -1,24 +1,21 @@
 require './utils.rb'
 
 a = read('9').lines.map &:to_i
-p1 = nil
 
+p1 = nil
 prev = a[0...25]
 a[25..].each{|x|
   if prev.combination(2).map(&:sum).count(x) == 0
     p1 = x
     break
   end
-  prev << x
-  prev.shift
+  prev = prev[1..] + [x]
 }
 
 p p1
 
-(0...a.size).each{|i|
-  ((i+1)...a.size).each{|j|
-    if a[i..j].sum == p1
-      p a[i..j].min + a[i..j].max
-    end
-  }
+(0...a.size).to_a.combination(2).each{|i,j|
+  if a[i..j].sum == p1
+    p a[i..j].min + a[i..j].max
+  end
 }
