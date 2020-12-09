806d36e091ca7d7078d46f7da441c57c30d1c77f
Henrik Aalto <hltk@hltk.fi>
2020-12-09 07:52:30 +0200

    one-liners

--- 2020/ruby/09.rb
+++ 2020/ruby/09.rb
@@ -2,18 +2,8 @@ require './utils.rb'
 
 a = read('9').lines.map &:to_i
 
-p1 = nil
-(25...a.size).each{|i|
-  if a[i-25...i].combination(2).map(&:sum).count(a[i]) == 0
-    p1 = a[i]
-    break
-  end
-}
-
+p1 = a.each_cons(26).find{|*v,x| !v.combination(2).map(&:sum).include? x}.last
 p p1
 
-(0...a.size).to_a.combination(2).each{|i,j|
-  if a[i..j].sum == p1
-    p a[i..j].min + a[i..j].max
-  end
-}
+i, j = (0...a.size).to_a.combination(2).find{|i,j| a[i..j].sum == p1}
+p a[i..j].minmax.sum
