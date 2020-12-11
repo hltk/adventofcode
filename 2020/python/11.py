79a404dd527d4da45132edf5c512ef9af1f790d0
Henrik Aalto <hltk@hltk.fi>
2020-12-11 10:23:55 +0200

    shorten 11 and improve speed

--- 2020/python/11.py
+++ 2020/python/11.py
@@ -1,37 +1,16 @@
 from utils import *
 from aocd import data
 
+
 grid = data.split("\n")
 n, m = len(grid), len(grid[0])
 
-def find_first(i, j, vi, vj, p1):
-    i += vi
-    j += vj
-    while i in range(n) and j in range(m):
-        if grid[i][j] != '.':
-            yield (i, j)
-            break
-        if p1:
-            break
-        i += vi
-        j += vj
 
-
-def neighbours(i, j, p1):
-    yield from find_first(i, j, -1, 0, p1)
-    yield from find_first(i, j, +1, 0, p1)
-    yield from find_first(i, j, 0, -1, p1)
-    yield from find_first(i, j, 0, +1, p1)
-    yield from find_first(i, j, +1, +1, p1)
-    yield from find_first(i, j, -1, +1, p1)
-    yield from find_first(i, j, +1, -1, p1)
-    yield from find_first(i, j, -1, -1, p1)
-
-def step(seats, p1):
+def step(seats, neigh, p1):
     def alive(i, j):
         if grid[i][j] == '.':
             return False
-        ncnt = sum((ni, nj) in seats for ni, nj in neighbours(i, j, p1))
+        ncnt = sum(map(seats.__contains__, neigh[(i, j)]))
         if (i, j) not in seats and ncnt == 0:
             return True
         if (i, j) in seats and ncnt < (4 if p1 else 5):
@@ -39,11 +18,32 @@ def step(seats, p1):
         return False
     return {(i, j) for i, j in product(range(n), range(m)) if alive(i, j)}
 
+
+def find_first(i, j, vi, vj, p1):
+    i, j = i + vi, j + vj
+    while i in range(n) and j in range(m):
+        if grid[i][j] != '.':
+            yield i, j
+            break
+        if p1:
+            break
+        i, j = i + vi, j + vj
+
+
+adj = set(product([-1, 0, 1], [-1, 0, 1])) - {(0, 0)}
+
+def neighbours(i, j, p1):
+    for vi, vj in adj:
+        yield from find_first(i, j, vi, vj, p1)
+
+
 def simulate(p1):
     seats = {(i, j) for i, j in product(range(n), range(m)) if grid[i][j] == '#'}
-    while (nxt := step(seats, p1)) != seats:
+    neigh = {p: list(neighbours(*p, p1)) for p in product(range(n), range(m))}
+    while (nxt := step(seats, neigh, p1)) != seats:
         seats = nxt
     return len(seats)
+
 
 print(simulate(True))
 
