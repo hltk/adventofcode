f6d3d72cf92ef4d29be8dc9362073c17537c8733
Henrik Aalto <hltk@hltk.fi>
2020-12-11 10:32:45 +0200

    Improve readability and variable names

--- 2020/python/11.py
+++ 2020/python/11.py
@@ -1,4 +1,4 @@
-from utils import *
+from itertools import product
 from aocd import data
 
 
@@ -6,28 +6,30 @@ grid = data.split("\n")
 n, m = len(grid), len(grid[0])
 
 
-def step(seats, neigh, p1):
+def step(cells, neigh, p1):
     def alive(i, j):
         if grid[i][j] == '.':
             return False
-        ncnt = sum(map(seats.__contains__, neigh[(i, j)]))
-        if (i, j) not in seats and ncnt == 0:
+        ncnt = sum(map(cells.__contains__, neigh[(i, j)]))
+        if (i, j) not in cells and ncnt == 0:
             return True
-        if (i, j) in seats and ncnt < (4 if p1 else 5):
+        if (i, j) in cells and ncnt < (4 if p1 else 5):
             return True
         return False
     return {(i, j) for i, j in product(range(n), range(m)) if alive(i, j)}
 
 
 def find_first(i, j, vi, vj, p1):
-    i, j = i + vi, j + vj
+    i += vi
+    j += vj
     while i in range(n) and j in range(m):
         if grid[i][j] != '.':
             yield i, j
             break
         if p1:
             break
-        i, j = i + vi, j + vj
+        i += vi
+        j += vj
 
 
 adj = set(product([-1, 0, 1], [-1, 0, 1])) - {(0, 0)}
@@ -38,11 +40,11 @@ def neighbours(i, j, p1):
 
 
 def simulate(p1):
-    seats = {(i, j) for i, j in product(range(n), range(m)) if grid[i][j] == '#'}
+    cells = {(i, j) for i, j in product(range(n), range(m)) if grid[i][j] == '#'}
     neigh = {p: list(neighbours(*p, p1)) for p in product(range(n), range(m))}
-    while (nxt := step(seats, neigh, p1)) != seats:
-        seats = nxt
-    return len(seats)
+    while (nxt := step(cells, neigh, p1)) != cells:
+        cells = nxt
+    return len(cells)
 
 
 print(simulate(True))
