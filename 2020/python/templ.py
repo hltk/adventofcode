f5cb6149a2e5ea6ce9a340f08b02e65bb9f180ee
Henrik Aalto <hltk@hltk.fi>
2020-12-14 20:22:52 +0200

    update templ.py and utils.py

--- 2020/python/templ.py
+++ 2020/python/templ.py
@@ -1,3 +1,22 @@
+from functools import *
+from itertools import *
+from more_itertools import *
+from parse import *
+from math import *
+from builtins import pow
+import operator
+import re
+
+def ints(line, non_neg=False):
+    reg = r"\d+" if non_neg else r"-?\d+"
+    return [int(x) for x in re.findall(reg, line)]
+
+# Taken from ecnerwala
+def cprint(ans):
+    print(ans)
+    import pyperclip
+    pyperclip.copy(ans)
+
 def main(inp):
     inp = inp.strip()
     inp = inp.split('\n')
--- 2020/python/utils.py
+++ 2020/python/utils.py
@@ -1,134 +1,37 @@
-import collections
-import copy
-import functools
-import itertools
-import math
-import more_itertools
-import networkx as nx
-import operator
-import re
-import sys
+# Interact with the queues using the get, put, empty methods
+#from queue import PriorityQueue, Queue,
 
-from itertools import (
-    accumulate,
-    chain,
-    combinations,
-    count,
-    cycle,
-    filterfalse,
-    groupby,
-    islice,
-    permutations,
-    product,
-    repeat,
-    starmap,
-    tee,
-    zip_longest,
-)
-
-from more_itertools import (
-    chunked,
-    consume,
-    first_true,
-    flatten,
-    grouper,
-    ilen,
-    ncycles,
-    nth,
-    pairwise,
-    partition,
-    powerset,
-    quantify,
-    repeatfunc,
-    roundrobin,
-    substrings,
-    substrings_indexes,
-    take,
-    unique_everseen,
-    unique_justseen,
-    unzip,
-    windowed,
-)
-
-from collections import (
-    Counter,
-    defaultdict,
-    deque,
-)
-
-
-# Interact with queue using get, put, empty
-
-from queue import (
-    PriorityQueue,
-    Queue,
-)
-
-from functools import (
-    partial,
-    reduce,
-)
-
-from math import (
-    ceil,
-    floor,
-    sqrt,
-)
-
-
-sys.setrecursionlimit(100000)
-
+#sys.setrecursionlimit(100000)
 
 def dotproduct(vec1, vec2):
     return sum(map(operator.mul, vec1, vec2))
 
+def zip_func(vec1, vec2, func):
+    return tuple(func(a, b) for a, b in zip(vec1, vec2))
 
-def vec_add(vec1, vec2):
-    return (a + b for a, b in zip(vec1, vec2))
+zip_add = partial(zip_func, func=operator.add)
+zip_sub = partial(zip_func, func=operator.sub)
+zip_mul = partial(zip_func, func=operator.mul)
 
-
-def vec_sub(vec1, vec2):
-    return (a - b for a, b in zip(vec1, vec2))
-
-
-def gen_grid(*dims, default=None):
+def tensor(*dims, default=None):
     if len(dims) == 1:
         return [default for x in range(dims[0])]
-    v = gen_grid(*dims[1:], default=default)
+    v = tensor(*dims[1:], default=default)
     return [copy.deepcopy(v) for _ in range(dims[0])]
-
-
-def ints(line, non_negative=False):
-    if non_negative:
-        reg = r"\d+"
-    else:
-        reg = r"-?\d+"
-    return [int(x) for x in re.findall(reg, line)]
-
 
 def starfilter(predicate, iterable):
     for t in iterable:
         if predicate(*t):
             yield t
 
-
 def neighbours(i, j):
     yield i - 1, j
     yield i + 1, j
     yield i, j - 1
     yield i, j + 1
+    #yield i - 1, j + 1
+    #yield i - 1, j - 1
+    #yield i + 1, j + 1
+    #yield i + 1, j - 1
 
-
-def valid_coords(i, j, n, m):
-    return 0 <= i < n and 0 <= j < m
-
-
-def adjacent_difference(it, f=operator.sub):
-    return starmap(lambda x, y: f(y, x), pairwise(it))
-
-
-# Stolen from ecnerwala
-def cprint(ans):
-    print(ans)
-    import pyperclip
-    pyperclip.copy(ans)
+valid_coords = lambda i, j: i in range(n) and j in range(m)
