277bc33deebc3373eb2de246f8907035e516f7a1
Henrik Aalto <hltk@hltk.fi>
2020-12-17 07:54:46 +0200

    make 17.py more compact and readable

--- 2020/python/17.py
+++ 2020/python/17.py
@@ -4,7 +4,6 @@ import operator
 
 def zip_func(vec1, vec2, func):
     return tuple(func(a, b) for a, b in zip(vec1, vec2))
-
 zip_add = partial(zip_func, func=operator.add)
 
 def neighbours(p):
@@ -16,13 +15,11 @@ def main(inp):
     n, m = len(inp), len(inp[0])
     act = {(i, j, 0, 0) for i, j in product(range(n), range(m)) if inp[i][j] == '#'}
     for t in range(6):
-        nact = set()
-        to_test = set(chain.from_iterable(neighbours(p) for p in act)) | act
-        for p in to_test:
+        def alive(p):
             cnt = sum(map(act.__contains__, neighbours(p)))
-            if cnt == 3 or (p in act and cnt == 2):
-                nact.add(p)
-        act = nact
+            return cnt == 3 or (p in act and cnt == 2)
+        to_test = set(chain.from_iterable(neighbours(p) for p in act)) | act
+        act = {p for p in to_test if alive(p)}
     print(len(act))
 
 from aocd import data
