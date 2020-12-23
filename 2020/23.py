426892b8e90b6f2e6b0addbf386187e08aaf9f3a
Henrik Aalto <hltk@hltk.fi>
2020-12-23 12:27:19 +0200

    solve day 23 in python

---
+++ 2020/23.py
@@ -0,0 +1,29 @@
+def main(inp):
+    inp = [int(x) - 1 for x in inp]
+    inp += list(range(max(inp) + 1, 10 ** 6))
+    nxt = [None for x in inp]
+    mx = max(inp) + 1
+    for a, b in zip(inp, inp[1:] + inp[:1]):
+        nxt[a] = b
+    cur = inp[0]
+    for _ in range(10 ** 7):
+        vals = []
+        orig = cur
+        cur = nxt[cur]
+        for _ in range(3):
+            vals.append(cur)
+            cur = nxt[cur]
+        nxt[orig] = cur
+        dest = orig
+        while True:
+            dest = (dest - 1 + mx) % mx
+            if dest not in vals:
+                break
+        nxt[vals[2]] = nxt[dest]
+        nxt[vals[1]] = vals[2]
+        nxt[vals[0]] = vals[1]
+        nxt[dest] = vals[0]
+    print((nxt[0] + 1) * (nxt[nxt[0]] + 1))
+
+from aocd import get_data
+main(get_data(day=23))
