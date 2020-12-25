6f247ca3f58d983548fe79213f6ea5db69d602fa
Henrik Aalto <hltk@hltk.fi>
2020-12-25 19:12:33 +0200

    update 2015/06.py

--- 2015/06.py
+++ 2015/06.py
@@ -13,9 +13,9 @@ def solve_1():
         a, b, c, d = map(int, re.findall(r'\d+', l))
         act = next(x for x in ["toggle", "on", "off"] if x in l)
         for i, j in product(range(a, c + 1), range(b, d + 1)):
-            if act == "on": g[(i, j)] = 1
-            if act == "off": g[(i, j)] = 0
-            if act == "toggle": g[(i, j)] = 1 - g[(i, j)]
+            if act == "on": g[i, j] = 1
+            if act == "off": g[i, j] = 0
+            if act == "toggle": g[i, j] = 1 - g[i, j]
 
     print(sum(x == 1for x in g.values()))
 
@@ -28,9 +28,9 @@ def solve_2():
         a, b, c, d = map(int, re.findall(r'\d+', l))
         act = next(x for x in ["toggle", "on", "off"] if x in l)
         for i, j in product(range(a, c + 1), range(b, d + 1)):
-            if act == "on": g[(i, j)] += 1
-            if act == "off": g[(i, j)] = max(g[(i, j)] - 1, 0)
-            if act == "toggle": g[(i, j)] += 2
+            if act == "on": g[i, j] += 1
+            if act == "off": g[i, j] = max(g[i, j] - 1, 0)
+            if act == "toggle": g[i, j] += 2
 
     print(sum(g.values()))
 
