f1c19d0d3fb5cb3b0124898547d88d9927ec6a68
Henrik Aalto <47699780+hltk@users.noreply.github.com>
2022-12-10 13:11:42 +0200

    improve 09

--- 2022/09.py
+++ 2022/09.py
@@ -1,38 +1,29 @@
 from aocd import data
 from aocd import lines
-from operator import add, methodcaller
+from operator import add, sub, methodcaller
 from more_itertools import flatten
 
 def norm(x): return x // abs(x) if x else 0
 def add_vec(a, b): return tuple(map(add, a, b))
+def sub_vec(a, b): return tuple(map(sub, a, b))
 
 mov = dict(zip("UDLR", [(1, 0), (-1, 0), (0, -1), (0, 1)]))
 
 make_moves = lambda line: [line.split()[0]] * int(line.split()[1])
 moves = list(flatten(map(make_moves, lines)))
 
-def move_tail(head, tail):
-    diff = head[0] - tail[0], head[1] - tail[1]
-    norm_diff = tuple(map(norm, diff))
-    if diff[1] == 0 and abs(diff[0]) > 1:
-        tail = add_vec(tail, (norm_diff[0], 0))
-    elif diff[0] == 0 and abs(diff[1]) > 1:
-        tail = add_vec(tail, (0, norm_diff[1]))
-    elif sum(map(abs, diff)) > 2:
-        tail = add_vec(tail, norm_diff)
-    return tail
-
-def simulate(n):
-    nodes = [(0, 0) for _ in range(n)]
-    tail_pos = {nodes[-1]}
+def simulate(*tails):
+    nodes = [[(0, 0) for _ in range(max(tails))]]
 
     for d in moves:
-        nodes[0] = add_vec(nodes[0], mov[d])
-        for i in range(1, n):
-            nodes[i] = move_tail(nodes[i - 1], nodes[i])
-        tail_pos.add(nodes[-1])
+        row = nodes[-1][:]
+        row[0] = add_vec(row[0], mov[d])
+        for i in range(1, len(row)):
+            diff = sub_vec(row[i - 1], row[i])
+            norm_diff = tuple(map(norm, diff))
+            row[i] = add_vec(row[i], norm_diff if max(map(abs, diff)) > 1 else (0, 0))
+        nodes.append(row)
 
-    return len(tail_pos)
+    return [*map(lambda i: len({*[*zip(*nodes)][i - 1]}), tails)]
 
-print(simulate(2))
-print(simulate(10))
+print(*simulate(2, 10))
