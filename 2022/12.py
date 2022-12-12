830a7dbd62176efacd58d70b8fcc7f1f3491adb3
Henrik Aalto <47699780+hltk@users.noreply.github.com>
2022-12-12 20:09:52 +0200

    clean up 12.py

--- 2022/12.py
+++ 2022/12.py
@@ -1,30 +1,27 @@
 from aocd import data
-from itertools import product
-from collections import defaultdict
+from itertools import product, starmap
 from utils import *
 
 grid = list(map(list, data.split("\n")))
 n, m = len(grid), len(grid[0])
+indices = {(i, j) for i, j in product(range(n), range(m))}
+grid = dict(zip(indices, starmap(lambda i, j: grid[i][j], indices)))
 
-q, vis, dist = [], set(), {}
+e ,= (p for p in indices if grid[p] == 'E')
+s ,= (p for p in indices if grid[p] == 'S')
 
-for i, j in product(range(n), range(m)):
-    if grid[i][j] == 'S':
-        e = i, j
-        grid[i][j] = 'a'
-    if grid[i][j] == 'E':
-        grid[i][j] = 'z'
-        q.append((i, j))
-        vis.add((i, j))
-        dist[i, j] = 0
+grid[e] = 'z'
+grid[s] = 'a'
 
-for i, j in iter(q):
-    for ni, nj in neighbours4(i, j):
-        if ni in range(n) and nj in range(m):
-            if (ni, nj) not in vis and ord(grid[i][j]) - ord(grid[ni][nj]) <= 1:
-                vis.add((ni, nj))
-                dist[ni, nj] = dist[i, j] + 1
-                q.append((ni, nj))
+q, vis, dist = [e], {e}, {e: 0}
 
-print(dist[e])
-print(min(v for (i, j), v in dist.items() if grid[i][j] == 'a'))
+for cur in iter(q):
+    for nxt in neighbours4(*cur):
+        if nxt in indices - vis:
+            if ord(grid[cur]) - ord(grid[nxt]) <= 1:
+                vis.add(nxt)
+                dist[nxt] = dist[cur] + 1
+                q.append(nxt)
+
+print(dist[s])
+print(min(v for p, v in dist.items() if grid[p] == 'a'))
