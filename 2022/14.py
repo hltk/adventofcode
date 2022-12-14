bcb9d28734fb9fece1bcef3f52c179b3a1f89a30
Henrik Aalto <47699780+hltk@users.noreply.github.com>
2022-12-14 22:35:32 +0200

    improve 14

--- 2022/14.py
+++ 2022/14.py
@@ -9,28 +9,18 @@ def points_from_line(line):
     points = list(starmap(V, zip(ints[::2], ints[1::2])))
     return set(flatten(starmap(V.interpolate, zip(points, points[1:]))))
 
-all_points = set.union(*map(points_from_line, lines))
-orig_points = {*all_points}
+moves, start = [(0, 1), (-1, 1), (1, 1)], V(500, 0)
 
-moves = [(0, 1), (-1, 1), (1, 1)]
-start = V(500, 0)
-inf = 1000
+points, placed = set.union(*map(points_from_line, lines)), set()
+exists = lambda p: p in placed or p in points
+floor = max(v.y for v in points) + 1
 
-while max(v.y for v in all_points) < inf:
+while start not in placed:
     sand = start
-    while sand.y < inf and (move := next(dropwhile(lambda x: (sand + x) in all_points, moves), None)):
+    while sand.y < floor and (move := next(dropwhile(lambda x: exists(sand + x), moves), None)):
         sand += move
-    all_points.add(sand)
+    if sand.y == floor and max(v.y for v in placed) < floor:
+        print(len(placed))
+    placed.add(sand)
 
-print(len(all_points - orig_points) - 1)
-
-all_points = {*orig_points}
-floor = max(v.y for v in all_points) + 2
-
-while start not in all_points:
-    sand = start
-    while sand.y + 1 < floor and (move := next(dropwhile(lambda x: (sand + x) in all_points, moves), None)):
-        sand += move
-    all_points.add(sand)
-
-print(len(all_points - orig_points))
+print(len(placed))
