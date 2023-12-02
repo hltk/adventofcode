34fc59b8129a17b0825dba1504b89053fc0e7c04
Henrik Aalto <47699780+hltk@users.noreply.github.com>
2023-12-02 07:59:24 +0200

    Improve 01

--- 2023/01.py
+++ 2023/01.py
@@ -4,14 +4,13 @@ for part in [1, 2]:
     numbers = "$ one two three four five six seven eight nine".split() if part == 2 else []
 
     def digit(x):
-        if x[0].isdigit(): return x[0]
         nums = (str(v) for v, n in enumerate(numbers) if x[:len(n)] == n)
-        return next(nums, None)
+        return x[0] if x[0].isdigit() else next(nums, None)
 
     ans = 0
 
     for l in data.split("\n"):
-        c = [x for i in range(len(l)) if (x := digit(l[i:]))]
+        c = [x for i, _ in enumerate(l) if (x := digit(l[i:]))]
         ans += int(c[0] + c[-1])
 
     print(ans)
