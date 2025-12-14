1468c80d040c598b263f20412141c98447454113
Henrik Aalto <henrik.aalto@iki.fi>
2025-12-14 22:03:10 +0200

    feat: improve 05

--- 2025/05.py
+++ 2025/05.py
@@ -5,13 +5,7 @@ f, d = data.split("\n\n")
 f = [*sorted((tuple(map(int, k.split("-"))) for k in f.split("\n")), key=lambda k: k[0])]
 
 
-def ok(x):
-    for a, b in f:
-        if a <= x <= b:
-            return True
-
-
-print(sum(not not ok(int(x)) for x in d.split("\n")))
+print(sum(any(a <= int(x) <= b for a, b in f) for x in d.split("\n")))
 
 
 r = ans = 0
