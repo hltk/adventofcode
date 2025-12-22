bea8f0be362d91430669b37dcdcb2b3695ee078a
Henrik Aalto <henrik.aalto@iki.fi>
2025-12-22 15:45:11 +0200

    feat: improve 04.py

--- 2025/04.py
+++ 2025/04.py
@@ -2,19 +2,19 @@ from aocd import data
 
 
 data = data.split("\n")
-g = {(i, j): d for i, r in enumerate(data) for j, d in enumerate(r)}
+g = {(i, j) for i, r in enumerate(data) for j, d in enumerate(r) if d == '@'}
 
 
 def access(p):
     i, j = p
-    return sum(g.get((i + di, j + dj), ".") == "@" for di in [-1, 0, 1] for dj in [-1, 0, 1]) < 5
+    return sum((i + di, j + dj) in g for di in [-1, 0, 1] for dj in [-1, 0, 1]) < 5
 
 
 a = []
 
 while not a or a[-1]:
-    a.append(len(x := [p for p, d in g.items() if d == "@" and access(p)]))
-    g |= {u: "." for u in x}
+    a.append(len(x := {p for p in g if access(p)}))
+    g -= x
 
 print(a[0])
 print(sum(a))
